bb2f57b99e6599931cf02cdeda24fa7c30fcec74
svdf18 <simonvandeurs@hotmail.com>
2024-05-15 09:52:13 +0200

    set<Ent> in InitData to avoid duplicate data

--- src/main/java/dat3/bxb/activity/Activity.java
+++ src/main/java/dat3/bxb/activity/Activity.java
@@ -1,8 +1,6 @@
 package dat3.bxb.activity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
